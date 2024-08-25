1e784ef55ec2066edbce6b9c10c1003878dd0e5d
FAHAD <eng.bokhari@hotmail.com>
2024-08-25 13:35:27 +1200

    - loop 5 time over whole process - Apply Filter-based feature selection. - Initialize population: initialize random indialization.

--- FilterGA.py
+++ FilterGA.py
@@ -26,6 +26,44 @@ def load_data(column_file_name, data_file_name):
 
     return dataset
 
+def FilterGA(feature, target):
+    """
+        CORRELATION CLACULATION: The corrwith method is used to calculate the correlation between each feature in X and the target variable y. 
+            This produces a series where each feature's correlation with the target is listed.
+
+        THRESHOLD: A threshold defined is 0.5, and only features with an absolute correlation greater than this threshold are selected.
+            This means we're interested in features that are strongly correlated with the target variable.
+        
+        DATA TRANSFORMATION: The selected features' names are extracted based on the threshold condition and stored in selected_features.
+            A new DataFrame X_selected is created, containing only the selected features that met the correlation threshold
+    """
+    threshold = 0.2
+
+    # Calculate the correlation between each feature and the target
+    correlations = feature.corrwith(target)
+
+    # Select features that have an absolute correlation value above the threshold
+    selected_features = correlations[abs(correlations) > threshold].index.tolist()
+
+    # Create a new DataFrame with only the selected features
+    #dataset_selected = dataset[selected_features]
+    return selected_features
+
+def initial_pop(population_size, feature_num, seed_val):
+    """
+    Generate the initial population for the genetic algorithm.
+
+    Args:
+        population_size (int): The desired size of the initial population.
+        feature_num (int): The number of the feature in dataset.
+
+    Returns:
+        list: A list of individuals, where each individual is a binary vector
+              representing a potential solution. The length of each individual is equal to `feature_num`.
+    """
+    random.seed(seed_val)
+    return [np.random.randint(2, size=feature_num) for _ in range(population_size)]
+
 def main():
     # Genetic operation setting 
     population_size = 50
@@ -44,6 +82,18 @@ def main():
     dataset = load_data(column_file, data_file)
     feature = dataset.iloc[:,:-1]
     target = dataset.iloc[:,-1]
+
+    for run in range(run_no):
+        start_time = time.time()  # Start timer for this run
+
+        # FEATURE FITNESS FUNCTION: 
+        FilterGA_fs = FilterGA(feature, target)   # Filter-based feature selection
+
+        # DATA TASFORMATION: create new dataset for each filter approach, created datset that only contain selected feature 
+        FilterGA_dataset = dataset[:][FilterGA_fs + ['class']]
+
+        # INITIALIZATION: Initialize the population 
+        FilterGA_population = initial_pop(population_size, FilterGA_dataset.iloc[:,:-1].shape[1], seed_value[run])
 
 
 
