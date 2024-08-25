9717ccab444eb47420461bd8e44d3ce0d177f303
FAHAD <eng.bokhari@hotmail.com>
2024-08-25 13:51:03 +1200

    - Apply Genetic Operation (Crossover, Mutation). - Apply Fitness Function (Classification). - Apply Reproduction new generation.

--- FilterGA.py
+++ FilterGA.py
@@ -4,6 +4,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import MinMaxScaler
 import time
 
 def load_data(column_file_name, data_file_name):
@@ -26,6 +27,47 @@ def load_data(column_file_name, data_file_name):
 
     return dataset
 
+def initial_pop(population_size, feature_num, seed_val):
+    """
+    Generate the initial population for the genetic algorithm.
+
+    Args:
+        population_size (int): The desired size of the initial population.
+        feature_num (int): The number of the feature in dataset.
+
+    Returns:
+        list: A list of individuals, where each individual is a binary vector
+              representing a potential solution. The length of each individual is equal to `feature_num`.
+    """
+    random.seed(seed_val)
+    return [np.random.randint(2, size=feature_num) for _ in range(population_size)]
+
+def normalize_data(dataset):
+    # Initialize and fit the scaler to dataset
+    scaler = MinMaxScaler() 
+    normalized_data = scaler.fit_transform(dataset) 
+    
+    # Convert the normalized data back into a DataFrame
+    normalized_dataset = pd.DataFrame(normalized_data, columns=dataset.columns) 
+
+    return normalized_dataset
+
+def classification(individual, dataset, target):
+    """
+        The fitness function evaluates the performance of the selected features:
+    """
+    selected_features = [index for index in range(len(individual)) if individual[index] == 1]
+    if len(selected_features) == 0:
+        return 0,
+    
+    X_selected = dataset[:, selected_features]
+    X_train, X_test, y_train, y_test = train_test_split(X_selected, target, test_size=0.3, random_state=42)
+    clf = RandomForestClassifier(n_estimators=50, random_state=42)
+    clf.fit(X_train, y_train)
+    predictions = clf.predict(X_test)
+
+    return accuracy_score(y_test, predictions)
+
 def FilterGA(feature, target):
     """
         CORRELATION CLACULATION: The corrwith method is used to calculate the correlation between each feature in X and the target variable y. 
@@ -47,22 +89,40 @@ def FilterGA(feature, target):
 
     # Create a new DataFrame with only the selected features
     #dataset_selected = dataset[selected_features]
+
     return selected_features
 
-def initial_pop(population_size, feature_num, seed_val):
+def crossover(parent1, parent2, crossover_rate):
     """
-    Generate the initial population for the genetic algorithm.
-
-    Args:
-        population_size (int): The desired size of the initial population.
-        feature_num (int): The number of the feature in dataset.
-
-    Returns:
-        list: A list of individuals, where each individual is a binary vector
-              representing a potential solution. The length of each individual is equal to `feature_num`.
+        The function chooses a random crossover point, 
+        then creates two new individuals by concatenating the genetic material of the two parents at that point
     """
-    random.seed(seed_val)
-    return [np.random.randint(2, size=feature_num) for _ in range(population_size)]
+    if np.random.rand() < crossover_rate:
+        num_features = parent1.shape[0]
+        crossover_point = np.random.randint(1, num_features - 1)
+        child1 = np.concatenate((parent1[:crossover_point], parent2[crossover_point:]))
+        child2 = np.concatenate((parent2[:crossover_point], parent1[crossover_point:]))
+        return child1, child2
+    else:
+        return parent1, parent2
+
+def mutation(individual, mutation_rate):
+    """
+    The function iterates over each element of the individual's genetic vector and, 
+    with probability mutation_rate, flips the element (i.e., changes 0 to 1 or 1 to 0)
+
+    PARAM: 
+        - individual (list): A list representing the genetic vector or chromosome of an individual in the population.
+        - items: A list of tuples, where each tuple represents an item with its weight and value (weight, value).
+    RETURN:
+        - mutated_individual (list): A new list representing the mutated individual's genetic vector after applying the mutation operation.
+    """
+    gens = individual.shape[0]
+    mutated_individual = individual.copy()
+    for i in range(gens):
+        if random.random() < mutation_rate:
+            mutated_individual[i] = 1 - mutated_individual[i]
+    return mutated_individual
 
 def main():
     # Genetic operation setting 
@@ -95,7 +155,24 @@ def main():
         # INITIALIZATION: Initialize the population 
         FilterGA_population = initial_pop(population_size, FilterGA_dataset.iloc[:,:-1].shape[1], seed_value[run])
 
+        # Apply Selection process
+        for generation in range(generations):
 
+            # EVALUATE: clasification and evaluates the performance of the selected features
+            FilterGA_fitness = [classification(individual, FilterGA_dataset.iloc[:,:-1].values, FilterGA_dataset.iloc[:,-1].values) for individual in FilterGA_population]
+
+            FilterGA_parents = np.array(FilterGA_population)[np.argsort(FilterGA_fitness)][-2:]
+
+            # CROSSOVER: Perform crossover to generate new offspring
+            FilterGA_offspring = crossover(FilterGA_parents[0], FilterGA_parents[1], crossover_rate)
+
+            # MUTATION: Perform mutation on the new offspring
+            FilterGA_offspring = [mutation(_, mutation_rate) for _ in FilterGA_offspring]
+
+            # REPLACEMENT: Replace the least fit individuals with the new offspring
+            FilterGA_population = np.vstack((FilterGA_population, FilterGA_offspring))
+
+            FilterGA_population = FilterGA_population[np.argsort(FilterGA_fitness)][:-2]
 
 if __name__ == "__main__":
     main()
